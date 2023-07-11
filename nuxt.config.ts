1862e774bdd749a5edc60f37989e81e0ffd4550b
Ahmad Moradi <=>
2023-07-12 01:24:57 +0330

    chore: Add tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
   css: ["@/assets/styles/main.css"],
   devtools: { enabled: true },
   modules: ["@pinia/nuxt"],
